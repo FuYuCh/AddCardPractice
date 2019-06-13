1ff942ae7a85ebff86edcec463bb3a1faaf3cb85
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-14 03:04:23 +0800

    Card類別建立

---
+++ AddCardPrictice/Assets/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card
+{
+    private int _number;
+    public float x;
+    private Sprite _mySprite;
+
+    public int Number
+    {
+        get => _number;
+    }
+
+    public float X
+    {
+        get => x;
+        set => x = value;
+    }
+
+    public Sprite mySprite
+    {
+        get => _mySprite;
+    }
+
+    public Card(int vNumber, float vX, Sprite vmySprite)
+    {
+        this._number = vNumber;
+        this.x = vX;
+        this._mySprite = vmySprite;
+    }
+
+   
+}
---
+++ AddCardPrictice/Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Texture2D mylmg;
+   
+    
+    private void Start()
+    {
+        
+       
+    }
+
+}
